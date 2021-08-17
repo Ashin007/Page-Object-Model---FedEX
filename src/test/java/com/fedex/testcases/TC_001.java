9e09e2772568a7fdfce27e344876d21c86701b61
Ashin <ashinvijayan007@gmail.com>
2021-08-18 00:15:35 +0530

    some console outpt

--- src/test/java/com/fedex/testcases/TC_001.java
+++ src/test/java/com/fedex/testcases/TC_001.java
@@ -4,6 +4,7 @@ import com.fedex.base.Base;
 import com.fedex.pages.HomePage;
 import com.fedex.pages.LanguagePage;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 //Open FedEX Web application
@@ -17,15 +18,23 @@ public class TC_001 extends Base {
     LanguagePage languagePage;
     HomePage homePage;
 
+    @BeforeTest
+    public void setUpFunction(){
+        System.out.println("Invoked Before Function");
+    }
+
     @Test
     public void cheeral_to_arizona(){
         driver = initialization();
+        System.out.println("Initialization");
         driver.get("https://www.fedex.com");
+        System.out.println("Opening URL");
         languagePage = new LanguagePage(driver);
         homePage = languagePage.clickOnEnglishLanguage();
         homePage.clickOnIndia();
         homePage.clickOnCovidCloseNotification();
         homePage.clickOnRate();
+        System.out.println("Quit Browser");
         driver.quit();
     }
 }
